745a7ea28c92edadcbfe28cbb8cd1f055de129b3
LUCAS MARTINI <10029452910@educacional.up>
2022-04-06 21:14:25 -0300

    produtos

---
+++ src/app/produtos/listar/listar.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { Produto } from 'src/models/produto.model';
+import { CadastroService } from 'src/services/cadastro.service';
+
+@Component({
+  selector: 'app-listar',
+  templateUrl: './listar.component.html',
+  styleUrls: ['./listar.component.css']
+})
+export class ListarComponent implements OnInit {
+
+  produtos: Produto[] = []
+
+  constructor(private cadastro: CadastroService) { }
+
+  ngOnInit(): void {
+    this.cadastro.listarProdutos().subscribe((produtos =>{
+      this.produtos = produtos;
+    }))
+  }
+
+}
